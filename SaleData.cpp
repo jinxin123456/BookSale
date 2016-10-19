979b49b47a15bef33a653c8bfeb8569ac6dca52d
jinxin <jinxin121204@163.com>
2016-10-19 11:02:01 +0800

    cpp

--- SaleData.cpp
+++ SaleData.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include "header.h"
+#include "Sale_item.h"
 
 using namespace std;
 
-istream& operator>>(istream &in,Sale_item & saledata)
+istream& Sale_item::operator>>(istream &in,Sale_item & saledata)
 {
 	double price;
 	in>>saledata.isbn>>saledata.units_sold>>price;
@@ -11,8 +11,23 @@ istream& operator>>(istream &in,Sale_item & saledata)
 		saledata.revenue=saledata.units_sold*price;
 	return in;
 }
-ostream& operator<<(ostream &out,const Sale_item & saledata)
+
+ostream& Sale_item::operator<<(ostream &out,const Sale_item & saledata)
 {
 	out<<saledata.isbn<<'\t'<<saladata<<units_sold<<'\t'<<saledata.revemue<<endl;
 	return out;
 }
+
+Sale_item Sale_item::operator+(const Sale_item &saladata1,const Sale_item &saladata2)
+{
+	Sale_item sum=saledata1;
+	sum+=saledata2;
+	return sum;
+}
+bool Sale_item::operator==(const Sale_item &saladata1,const Sale_item &saladata2)
+{
+	return saledata1.isbn==saledata2.isbn && 
+		saledata1.units_sold==saladata2.units_sold &&
+		saledata1.revenue==saledata2.revenue;
+}
+
